00cbd9b5370fd2d80dde398d202568638390eb75
kidffiti <coreyash10@gmail.com>
2019-08-28 12:29:04 -0400

    edit profile controller done

--- client/src/components/pages/MyProfile/MyProfile.jsx
+++ client/src/components/pages/MyProfile/MyProfile.jsx
@@ -33,17 +33,17 @@ class MyProfile extends Component {
     const file = event.target.files[0]
     this.setState({ selectedFile: file })
   }
-  
+
   uploadHandler = () => { console.log(this.state.selectedFile) }
 
   componentDidMount() {
     let key = localStorage.getItem('token')
-    this.setState({token: key})
+    this.setState({ token: key })
     axios.get(`/me/` + key).then(res => {
-        if(res.data.isChef === true){
-            this.setState({ isChef: true})
-        }
-        console.log(this.state)
+      if (res.data.isChef === true) {
+        this.setState({ isChef: true })
+      }
+      console.log(this.state)
     })
 
   }
@@ -56,13 +56,13 @@ class MyProfile extends Component {
     console.log(this.state.selectedFile)
     const formData = new FormData()
     formData.append(
-        'myFile',
-        this.state.selectedFile,
-        this.state.selectedFile.name
+      'myFile',
+      this.state.selectedFile,
+      this.state.selectedFile.name
     )
-    for(var pair of formData.entries()) {
-        console.log(pair[0]+ ', '+ pair[1].name); 
-     }
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ', ' + pair[1].name);
+    }
 
     // axios.post(`/editprofile`, {
     //     headers: {
@@ -91,8 +91,8 @@ class MyProfile extends Component {
     //   const loc = window.location.pathname
     //   const dir = loc.substring(0, loc.lastIndexOf('/'));
     //   window.location.pathname = dir + '/FindAMeal'
-      // this.setState({redirect: true})
-      // this.renderRedirect();
+    // this.setState({redirect: true})
+    // this.renderRedirect();
     // })
   };
   // renderRedirect = () => {
@@ -106,89 +106,138 @@ class MyProfile extends Component {
     //   return <Redirect to="/FindAMeal" />
     // } else
     return (
-        <div className="MyProfile">
-            <form  className="form-group" action={"/editprofile/" + this.state.token} enctype="multipart/form-data" method="POST">
-            <input type="file" name="myprofile" onChange={this.fileChangedHandler} />
-            <button type ="submit">Upload!</button>
-            </form>
-        </div>
-    //   <div className="ClientSignUp">
-    //     <div className="column col-4 col-xs-12">
-    //       <form className="form-group" onSubmit={this.handleSubmit}>
-    //         <label htmlFor="firstName">Enter your First Name: </label>
-    //         <input
-    //           class="form-input"
-    //           id="firstname"
-    //           name="firstname"
-    //           type="text"
-    //           value={this.state.firstname}
-    //           onChange={this.handleChange}
-    //         />
-    //         <div className="form-group">
-    //         <label htmlFor="lastName">Enter your Last Name: </label>
-    //         <input
-    //           class="form-input"
-    //           id="lastname"
-    //           name="lastname"
-    //           type="text"
-    //           value={this.state.lastname}
-    //           onChange={this.handleChange}
-    //         />
+      <div className="MyProfile">
+        <form className="form-group" action={"/upload/" + this.state.token} enctype="multipart/form-data" method="POST">
+          <div className="form-group">
+            <label htmlFor="image">Upload a Profile Picture:</label>
+            <div>
+              <input type="file" name="image" onChange={this.fileChangedHandler} />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="chefQualifications">Select Your Experience:</label>
+            <select className="form-select"
+              id="chefQualifications"
+              name="chefQualifications"
+              type="text"
+              value={this.state.chefQualifications}
+              onChange={this.handleChange}
+            >
+              <option selected disabled>Choose an option</option>
+              <option>Homemade Recipes</option>
+              <option>Rookie Chef</option>
+              <option>Veteran Chef</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="chefBio">Enter a Brief Biography:</label>
+            <textarea className="form-input"
+              rows="3"
+              id="chefBio"
+              name="chefBio"
+              type="text"
+              value={this.state.chefBio}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="chefRate">Price Range: $-$$$$</label>
+            <select className="form-select"
+              id="chefRate"
+              name="chefRate"
+              type="text"
+              value={this.state.chefRate}
+              onChange={this.handleChange}
+            >
+              <option selected disabled>Choose an option</option>
+              <option>$</option>
+              <option>$$</option>
+              <option>$$$</option>
+              <option>$$$$</option>
+            </select>
+          </div>
+          <div>
+            <button type="submit">Create Profile!</button>
+          </div>
+        </form>
+      </div>
+      //   <div className="ClientSignUp">
+      //     <div className="column col-4 col-xs-12">
+      //       <form className="form-group" onSubmit={this.handleSubmit}>
+      //         <label htmlFor="firstName">Enter your First Name: </label>
+      //         <input
+      //           class="form-input"
+      //           id="firstname"
+      //           name="firstname"
+      //           type="text"
+      //           value={this.state.firstname}
+      //           onChange={this.handleChange}
+      //         />
+      //         <div className="form-group">
+      //         <label htmlFor="lastName">Enter your Last Name: </label>
+      //         <input
+      //           class="form-input"
+      //           id="lastname"
+      //           name="lastname"
+      //           type="text"
+      //           value={this.state.lastname}
+      //           onChange={this.handleChange}
+      //         />
 
-    //         </div>
-    //         <div className="form-group">
-    //         <label htmlFor="phoneNumber">Enter your Phone Number: </label>
-    //         <input
-    //           class="form-input"
-    //           id="phone"
-    //           name="phone"
-    //           type="number"
-    //           value={this.state.phone}
-    //           onChange={this.handleChange}
-    //         />
-    //         </div>
-    //         <div className="form-group">
-    //         <label htmlFor="email">Enter your email: </label>
-    //         <input
-    //           class="form-input"
-    //           id="email"
-    //           name="email"
-    //           type="email"
-    //           value={this.state.email}
-    //           onChange={this.handleChange}
-    //         />
-    //         </div>
-    //         <div className="form-group">
-    //         <label htmlFor="username">Enter your username: </label>
-    //         <input
-    //           class="form-input"
-    //           id="username"
-    //           name="username"
-    //           type="text"
-    //           value={this.state.username}
-    //           onChange={this.handleChange}
-    //         />
-    //         </div>
-    //         <div className="form-group">
-    //         <label htmlFor="password">Enter your password: </label>
-    //         <input
-    //           class="form-input"
-    //           id="password"
-    //           name="password"
-    //           type="password"
-    //           value={this.state.password}
-    //           onChange={this.handleChange}
-    //         />
-    //         </div>
-    //         <label className="form-switch">
-    //           <input id="isChef" type="checkbox" name="isChef" onChange={this.handleChef} />
-    //           <i className="form-icon"></i> Are you a chef?
-    //           </label>
-    //         <button type="submit">Submit</button>
-    //       </form>
+      //         </div>
+      //         <div className="form-group">
+      //         <label htmlFor="phoneNumber">Enter your Phone Number: </label>
+      //         <input
+      //           class="form-input"
+      //           id="phone"
+      //           name="phone"
+      //           type="number"
+      //           value={this.state.phone}
+      //           onChange={this.handleChange}
+      //         />
+      //         </div>
+      //         <div className="form-group">
+      //         <label htmlFor="email">Enter your email: </label>
+      //         <input
+      //           class="form-input"
+      //           id="email"
+      //           name="email"
+      //           type="email"
+      //           value={this.state.email}
+      //           onChange={this.handleChange}
+      //         />
+      //         </div>
+      //         <div className="form-group">
+      //         <label htmlFor="username">Enter your username: </label>
+      //         <input
+      //           class="form-input"
+      //           id="username"
+      //           name="username"
+      //           type="text"
+      //           value={this.state.username}
+      //           onChange={this.handleChange}
+      //         />
+      //         </div>
+      //         <div className="form-group">
+      //         <label htmlFor="password">Enter your password: </label>
+      //         <input
+      //           class="form-input"
+      //           id="password"
+      //           name="password"
+      //           type="password"
+      //           value={this.state.password}
+      //           onChange={this.handleChange}
+      //         />
+      //         </div>
+      //         <label className="form-switch">
+      //           <input id="isChef" type="checkbox" name="isChef" onChange={this.handleChef} />
+      //           <i className="form-icon"></i> Are you a chef?
+      //           </label>
+      //         <button type="submit">Submit</button>
+      //       </form>
 
-    //     </div>
-    //   </div>
+      //     </div>
+      //   </div>
     );
 
   }
--- server/controllers/chef-controller.js
+++ server/controllers/chef-controller.js
@@ -5,10 +5,10 @@ const router = express.Router();
 // const upload = multer({ dest: '../uploads'})
 const path = require(`path`);
 const clientDir = path.join(__dirname, `../../client`);
-const {config} = require('cloudinary')
-const { uploader, cloudinaryConfig }=require('../config/cloudinaryConfig') 
-const {multerUploads, dataUri} = require('../middleware/multer')
-const {User, AuthToken, chefTable} = require("../models");
+const { config } = require('cloudinary')
+const { uploader, cloudinaryConfig } = require('../config/cloudinaryConfig')
+const { multerUploads, dataUri } = require('../middleware/multer')
+const { User, AuthToken, chefTable } = require("../models");
 
 // const storage = multer.diskStorage({
 //     destination: (req, file, cb) => {
@@ -30,32 +30,111 @@ router.get('/allchefs', async (req, res) => {
         })
 })
 
-router.post('/upload', multerUploads, (req, res) => {
+router.post('/upload/:token', multerUploads, (req, res) => {
     console.log(req.file)
-    if(req.file) {
-     const file = dataUri(req).content;
-     return uploader.upload(file).then((result) => {
-       const image = result.url;
-       return res.status(200).json({
-         messge: 'Your image has been uploded successfully to cloudinary',
-         data: {
-           image
-         }
-       })
-     }).catch((err) => res.status(400).json({
-       messge: 'someting went wrong while processing your request',
-       data: {
-         err
-       }
-     }))
+    console.log(req.body)
+    const usertoken = req.params.token
+    let rate = req.body.chefRate
+    switch (rate) {
+        case '$':
+            rate = 1
+            break;
+        case '$$':
+            rate = 2
+            break;
+        case '$$$':
+            rate = 3
+            break;
+        case '$$$$':
+            rate = 4
+            break;
     }
-   });
+    AuthToken.findOne({
+        where: {
+            token: usertoken
+        },
+        attributes: ['UserId']
+    }).then(user => {
+        console.log("User ID : " + user.UserId)
+        const userId = user.UserId
+        chefTable.findOne({
+            where: {
+                UserId: userId
+            }
+        }).then(chef => {
+            if (chef === null) {
+                if (req.file) {
+                    const file = dataUri(req).content;
+                    return uploader.upload(file).then((result) => {
+                        const image = result.url;
+                        chefTable.create({
+                            UserId: userId,
+                            chefProfilePictureURL: image,
+                            chefQualifications: req.body.chefQualifications,
+                            chefBio: req.body.chefBio,
+                            chefRate: rate
+                        })
+                        return res.redirect('/EditProfile')
+                    }).catch((err) => res.status(400).json({
+                        messge: 'someting went wrong while processing your request',
+                        data: {
+                            err
+                        }
+                    }))
+                } else if (req.file == undefined) {
+                    chefTable.create({
+                        UserId: userId,
+                        chefQualifications: req.body.chefQualifications,
+                        chefBio: req.body.chefBio,
+                        chefRate: rate
+                    })
+                    return res.redirect('/EditProfile')
+                }
+            } else {
+                if (req.file) {
+                    const file = dataUri(req).content;
+                    return uploader.upload(file).then((result) => {
+                        const image = result.url;
+                        chefTable.update(
+                            {
+                            chefProfilePictureURL: image,
+                            chefQualifications: req.body.chefQualifications,
+                            chefBio: req.body.chefBio,
+                            chefRate: rate
+                        }, {
+                            where: {
+                                UserId: userId
+                            }
+                        })
+                        return res.redirect('/EditProfile')
+                    }).catch((err) => res.status(400).json({
+                        messge: 'someting went wrong while processing your request',
+                        data: {
+                            err
+                        }
+                    }))
+                } else if (req.file == undefined) {
+                    chefTable.update({
+                        chefQualifications: req.body.chefQualifications,
+                        chefBio: req.body.chefBio,
+                        chefRate: rate
+                    }, {
+                        where : {
+                        UserId: userId
+                    }})
+                    return res.redirect('/EditProfile')
+                }
+            }
+        })
+    })
+
+});
 
 // router.post('/editprofile/:token', multer({storage: storage}).single('myprofile'),  async (req, res) => {
 //     console.log(req.file)
 //     const filePath = `${req.file.destination}/${req.file.filename}`
 //     console.log(filePath)
-    
+
 
 //     const usertoken = req.params.token
 //     AuthToken.findOne({
